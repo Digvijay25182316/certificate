297fe7368497728970735f8bc64386447207b0ff
Digvijay Edake <digvijays.edake>
2022-11-20 00:03:27 +0530

    commited

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-const express = require("express");
-
-app.use("/public", express.static(`${__dirname}/assets`));
-
-app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/html-file/error.html`);
-});
